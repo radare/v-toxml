852df288905d05c3b2015ce88798e2b4fb369b37
pancake <pancake@nopcode.org>
2020-05-25 01:07:48 +0200

    Add the example from the readme as a test

--- toxml_test.v
+++ toxml_test.v
@@ -1,8 +1,51 @@
 module toxml
 
-fn toxml_test() {
+
+fn test_toxml_comment() {
 	x := toxml.new()
 	x.comment('hello world')
 	x.finish()
-	println(x.str())
+	expect := '<!-- hello world -->'
+	assert(x.str().trim_space() == expect)
+}
+
+fn test_toxml_nest() {
+	x := toxml.new()
+	x.prolog('xml', {
+		'version': '1.0'
+		'encoding': 'UTF-8'
+	})
+	x.open('users', {
+		'total': '2'
+	})
+
+	x.open('user', {
+		'name': 'pancake'
+		'password': '1234'
+	})
+	x.body('/home/pancake')
+	x.close()
+
+	x.open('user', {
+		'name': 'root'
+		'password': 'toor'
+	})
+	x.body('/root')
+	x.close()
+
+	x.comment('hello world')
+	x.finish()
+
+	expect := '<?xml version="1.0" encoding="UTF-8"?>
+<users total="2">
+  <user name="pancake" password="1234">
+/home/pancake
+  </user>
+  <user name="root" password="toor">
+/root
+  </user>
+  <!-- hello world -->
+</users>
+'
+	assert(x.str() == expect)
 }
