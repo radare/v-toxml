5b2ea82b3248e7a7ac93ad772fe22311695032bd
pancake <pancake@nopcode.org>
2020-05-29 17:09:09 +0200

    Autogenerate the README with code

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 all:
 	v test .
 	cd examples && for a in *.v ; do v run $$a ; done
+
+README.md:
+	cd examples && v run readme.v > ../README.md
--- examples/readme.v
+++ examples/readme.v
@@ -1,4 +1,8 @@
+import toxml
+import os
 
+const (
+header = '
 XML Serialization library for V
 ===============================
 
@@ -8,9 +12,35 @@ Usage example:
 
 ```go
 import toxml
+'
+
+footer = '
+```
+The output for this code is the following:
+
+```xml
+'
+)
+
+fn main_code() string {
+	contents := os.read_file(@FILE) or { panic(err) }
+	fn_main_index := contents.index('fn main' + '() {') or { panic(err) }
+	fn_main := contents[fn_main_index..]
+	lines := fn_main.split('\n')
+	mut res := []string{}
+	for line in lines {
+		if line.contains('println') && !line.contains('(x)') {
+			continue
+		}
+		res << line
+	}
+	return res.join('\n')
+}
 
 fn main() {
 	mc := main_code()
+	println(header)
+	println(mc)
 	x := toxml.new()
 	x.prolog('xml', {
 		'version': '1.0'
@@ -41,25 +71,7 @@ fn main() {
 	x.close()
 	x.finish()
 
+	println(footer)
 	println(x)
+	println('``' + '`\n')
 }
-
-
-```
-The output for this code is the following:
-
-```xml
-
-<?xml version="1.0" encoding="UTF-8"?>
-<testsuites id="20150612_170402" name="new-config" tests="1234" failures="1234" time="1234">
-  <testsuite id="20150612_170402" name="new-config" time="1234">
-    <testcase id="20150612_170402" name="new-config" time="1234">
-      <failure message="Warning message here" type="WARNING">
-Warning
-      </failure>
-    </testcase>
-  </testsuite>
-</testsuites>
-
-```
-
