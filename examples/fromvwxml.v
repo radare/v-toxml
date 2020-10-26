080a4a85bc8fba870459d7a1d777cf1ed41ac938
pancake <pancake@nopcode.org>
2020-10-26 23:11:01 +0100

    Fix build

--- examples/fromvwxml.v
+++ examples/fromvwxml.v
@@ -1,7 +1,7 @@
 import toxml // serialize
 import zenith391.vwxml.xml // deserialize
 
-fn walk_into(toxml &toxml.Toxml, node &xml.Node) {
+fn walk_into(mut toxml toxml.Toxml, node &xml.Node) {
         for t in node.childrens {
 		mut attrs := map[string]string{}
                 for a in t.attributes {
@@ -9,16 +9,15 @@ fn walk_into(toxml &toxml.Toxml, node &xml.Node) {
                 }
 		toxml.open(t.name, attrs)
 		toxml.body(t.text)
-		walk_into(toxml, &t)
+		walk_into(mut toxml, &t)
 		toxml.close()
         }
 }
 
 fn main() {
         node := xml.parse('<thing abc="test"><test>Hello</test><test uid="123">World</test></thing>')
-	out := toxml.new()
-	walk_into(out, node)
+	mut out := toxml.new()
+	walk_into(mut out, node)
 	res := out.str()
 	println('$res')
-	
 }
